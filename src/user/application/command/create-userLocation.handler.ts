4407958d2dd2515cfd53ca35118e93278dc7e5c9
iysh321 <iysh321@gmail.com>
2024-03-31 23:33:41 +0900

    update: userLocation 중복 저장, 3개 초과 저장 제한

--- src/user/application/command/create-userLocation.handler.ts
+++ src/user/application/command/create-userLocation.handler.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, HttpException, Inject, Injectable } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 
 import { CreateUserLocationCommand } from './create-userLocation.command';
@@ -16,9 +16,26 @@ export class CreateUserLocationHandler implements ICommandHandler<CreateUserLoca
   async execute(command: CreateUserLocationCommand) {
     const { userId, locationIds } = command;
 
-    // 중복 저장 방지 로직
+    // locationId 확인
     await this.locationService.findByIds(locationIds);
 
+    const userLocation = await this.userLocationRepository.findByUserId(userId);
+
+    // 중복 저장 방지 로직
+    if (userLocation) {
+      if (userLocation.length === 3) {
+        throw new ForbiddenException('관심지역을 3개 초과하여 저장 할 수 없습니다.');
+      }
+
+      const set = new Set(locationIds);
+      const duplicate = userLocation.filter((value) => set.has(value.locationId)).map((value) => value.locationId);
+
+      if (duplicate.length > 0) {
+        throw new ForbiddenException(`${duplicate}는 중복된 locationId 입니다.`);
+      }
+    }
+
+    // 저장
     const result = await this.userLocationRepository.bulkInsert(userId, locationIds);
 
     return result;
