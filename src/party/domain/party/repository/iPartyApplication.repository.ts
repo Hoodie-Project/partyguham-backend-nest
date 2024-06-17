6168ab0f9274d3c1303bbba9f1c94389c367b9f9
iysh321 <iysh321@gmail.com>
2024-06-17 17:48:08 +0900

    upgrade: party application 중복 방지

--- src/party/domain/party/repository/iPartyApplication.repository.ts
+++ src/party/domain/party/repository/iPartyApplication.repository.ts
@@ -4,6 +4,7 @@ export interface IPartyApplicationRepository {
   create: (userId: number, partyId: number, message: string) => Promise<PartyApplicationEntity>;
   findOne: (partyId: number) => Promise<PartyApplicationEntity>;
   findAll: (partyId: number) => Promise<PartyApplicationEntity[]>;
+  findOneByUserIdAndPartyRecruitmentId: (userId: number, partyRecruitmentId: number) => Promise<PartyApplicationEntity>;
   update: (partyId: number, title: string, content: string) => Promise<void>;
   delete: (partyId: number) => Promise<void>;
 }
