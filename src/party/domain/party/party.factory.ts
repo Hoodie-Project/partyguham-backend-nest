a2d570a5d55b69a3d7fbc8f91c242e82dfecbf95
iysh321 <iysh321@gmail.com>
2024-06-28 16:37:45 +0900

    update: party factory

--- src/party/domain/party/party.factory.ts
+++ src/party/domain/party/party.factory.ts
@@ -4,6 +4,7 @@ import { Party } from './party';
 
 interface PartyData {
   id: number;
+  partyTypeId: number;
   title: string;
   content: string;
   image: string;
@@ -14,11 +15,11 @@ export class PartyFactory {
   constructor(private eventBus: EventBus) {}
 
   create(data: PartyData): Party {
-    const { id, title, content, image } = data;
-    return new Party(id, title, content, image);
+    const { id, partyTypeId, title, content, image } = data;
+    return new Party(id, partyTypeId, title, content, image);
   }
 
-  reconstitute(id: number, title: string, content: string, image: string): Party {
-    return new Party(id, title, content, image);
+  reconstitute(id: number, partyTypeId: number, title: string, content: string, image: string): Party {
+    return new Party(id, partyTypeId, title, content, image);
   }
 }
