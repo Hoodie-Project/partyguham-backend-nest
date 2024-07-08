76cbb7a7f5c71f0056203bc484cf8de83f474b10
iysh321 <iysh321@gmail.com>
2024-07-08 10:50:23 +0900

    update: postion dto

--- src/position/dto/response/position.response.dto.ts
+++ src/position/dto/response/position.response.dto.ts
@@ -4,16 +4,17 @@ import { Exclude, Expose } from 'class-transformer';
 @Exclude()
 export class PositionResponseDto {
   @Expose()
-  @ApiProperty({ example: 1 })
+  @ApiProperty({ example: 1, description: 'position ID (PK - 포지션)' })
   id: number;
 
   @Expose()
-  @ApiProperty({ example: '기획' })
+  @ApiProperty({ example: '기획', description: '기획, 디자인, 개발, 마케터/광고' })
   main: string;
 
   @Expose()
   @ApiProperty({
     example: 'UI/UX 기획자',
+    description: '  UI/UX 기획자, PM (프로젝트 매니저), PO (프로덕트 오너), 서비스 기획자 ...',
   })
   sub: string;
 }
