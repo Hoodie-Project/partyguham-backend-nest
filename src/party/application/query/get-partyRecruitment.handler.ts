14a5b2241a3605ab97168869768131b646058784
iysh321 <iysh321@gmail.com>
2024-09-09 01:23:41 +0900

    feat: 모집 조회 API  query 추가

--- src/party/application/query/get-partyRecruitment.handler.ts
+++ src/party/application/query/get-partyRecruitment.handler.ts
@@ -11,15 +11,21 @@ export class GetPartyRecruitmentHandler implements IQueryHandler<GetPartyRecruit
   constructor(@InjectRepository(PartyEntity) private partyRepository: Repository<PartyEntity>) {}
 
   async execute(query: GetPartyRecruitmentQuery) {
-    const { partyId } = query;
+    const { partyId, sort, order, main } = query;
 
-    const party = await this.partyRepository
+    const partyQuery = await this.partyRepository
       .createQueryBuilder('party')
       .leftJoinAndSelect('party.partyRecruitments', 'partyRecruitments')
       .leftJoinAndSelect('partyRecruitments.position', 'position')
       .leftJoinAndSelect('partyRecruitments.partyApplications', 'partyApplications')
       .where('party.id = :id', { id: partyId })
-      .getOne();
+      .addOrderBy(`partyUser.${sort}`, order);
+
+    if (main !== undefined && main !== null) {
+      partyQuery.andWhere('position.main = :main', { main });
+    }
+
+    const party = await partyQuery.getOne();
 
     if (!party) {
       throw new NotFoundException('파티가 존재하지 않습니다', 'PARTY_NOT_EXIST');
---
+++ src/party/interface/dto/request/partyRecruitment.query.request.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsIn, IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class PartyRecruitmentQueryRequestDto {
+  @ApiProperty({
+    enum: ['createdAt'],
+    description: `order에 대한 조회 기준 (default = createdAt) 
+    createdAt = 생성순
+    `,
+  })
+  @IsIn(['createdAt'])
+  @IsString()
+  @IsNotEmpty()
+  public sort: string;
+
+  @ApiProperty({
+    enum: ['ASC', 'DESC'],
+    description: `sort에 대한 조회 방법 (default = DESC)
+    ASC = 오름 
+    DESC = 내림차순`,
+  })
+  @IsIn(['ASC', 'DESC'])
+  @IsString()
+  @IsNotEmpty()
+  public order: 'ASC' | 'DESC';
+
+  @ApiPropertyOptional({
+    enum: ['기획자', '디자이너', '개발자', '마케터/광고'],
+    description: '직군 조건 조회 (선택 옵션)',
+  })
+  @IsIn(['기획자', '디자이너', '개발자', '마케터/광고'])
+  @IsString()
+  @IsOptional()
+  public main: string;
+}
