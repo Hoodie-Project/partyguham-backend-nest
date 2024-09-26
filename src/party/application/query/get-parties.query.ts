6aeb44ee0a69ce33dcae0bb49901956569b78ce5
iysh321 <iysh321@gmail.com>
2024-09-26 20:58:03 +0900

    feat: 파티 목록 조회 API 검색,필터 추가

--- src/party/application/query/get-parties.query.ts
+++ src/party/application/query/get-parties.query.ts
@@ -6,5 +6,8 @@ export class GetPartiesQuery implements IQuery {
     readonly limit: number,
     readonly sort: string,
     readonly order: 'ASC' | 'DESC',
+    readonly status: string,
+    readonly partyType: string,
+    readonly titleSearch: string,
   ) {}
 }
