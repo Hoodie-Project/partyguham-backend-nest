c7b859054e82d9169c470cc404fd246310e41410
iysh321 <iysh321@gmail.com>
2024-11-20 23:07:43 +0900

    fix: get party api - accesstoken 제거

--- src/party/application/query/get-party.query.ts
+++ src/party/application/query/get-party.query.ts
@@ -1,8 +1,5 @@
 import { IQuery } from '@nestjs/cqrs';
 
 export class GetPartyQuery implements IQuery {
-  constructor(
-    readonly partyId: number,
-    readonly userId: number | null,
-  ) {}
+  constructor(readonly partyId: number) {}
 }
