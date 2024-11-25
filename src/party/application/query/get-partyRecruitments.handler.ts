785dd3973942093558749cdd883ffb1913f4e0c5
iysh321 <iysh321@gmail.com>
2024-11-25 22:31:23 +0900

    fix: 파티모집 공고 조회시 오류 수정
    파티가 없음에도 빈 배열 리턴 수정 / 404 리턴으로 변경

--- src/party/application/query/get-partyRecruitments.handler.ts
+++ src/party/application/query/get-partyRecruitments.handler.ts
@@ -1,21 +1,33 @@
 import { NotFoundException } from '@nestjs/common';
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { InjectRepository } from '@nestjs/typeorm';
-
 import { Repository } from 'typeorm';
+
 import { GetPartyRecruitmentsQuery } from './get-partyRecruitments.query';
+
+import { PartyEntity } from 'src/party/infra/db/entity/party/party.entity';
 import { PartyRecruitmentEntity } from 'src/party/infra/db/entity/apply/party_recruitment.entity';
 
 @QueryHandler(GetPartyRecruitmentsQuery)
 export class GetPartyRecruitmentsHandler implements IQueryHandler<GetPartyRecruitmentsQuery> {
   constructor(
+    @InjectRepository(PartyEntity) private partyRepository: Repository<PartyEntity>,
     @InjectRepository(PartyRecruitmentEntity) private partyRecruitmentRepository: Repository<PartyRecruitmentEntity>,
   ) {}
 
   async execute(query: GetPartyRecruitmentsQuery) {
     const { partyId, sort, order, main } = query;
 
-    const partyQuery = this.partyRecruitmentRepository
+    const party = await this.partyRepository
+      .createQueryBuilder('party')
+      .where('party.id = :id', { id: partyId })
+      .getOne();
+
+    if (!party) {
+      throw new NotFoundException('파티가 존재하지 않습니다', 'PARTY_NOT_EXIST');
+    }
+
+    const partyRecruitmentQuery = this.partyRecruitmentRepository
       .createQueryBuilder('partyRecruitments')
       .leftJoin('partyRecruitments.position', 'position')
       .select([
@@ -32,15 +44,11 @@ export class GetPartyRecruitmentsHandler implements IQueryHandler<GetPartyRecrui
       .orderBy(`partyRecruitments.${sort}`, order);
 
     if (main !== undefined && main !== null) {
-      partyQuery.andWhere('position.main = :main', { main });
+      partyRecruitmentQuery.andWhere('position.main = :main', { main });
     }
 
-    const party = await partyQuery.getMany();
+    const partyRecruitments = await partyRecruitmentQuery.getMany();
 
-    if (!party) {
-      throw new NotFoundException('파티가 존재하지 않습니다', 'PARTY_NOT_EXIST');
-    }
-
-    return party;
+    return partyRecruitments;
   }
 }
