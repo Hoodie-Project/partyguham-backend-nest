63c8cf317e697f21d416b57e3771bee6c8080fbc
iysh321 <iysh321@gmail.com>
2024-12-06 22:55:59 +0900

    update: 내가 속한 파티 조회시 status 추가

--- src/user/application/query/get-myParties.handler.ts
+++ src/user/application/query/get-myParties.handler.ts
@@ -29,7 +29,7 @@ export class GetMyPartiesHandler implements IQueryHandler<GetMyPartiesQuery> {
         'party.id',
         'party.title',
         'party.image',
-        'party.createdAt',
+        'party.status',
         'partyType.type',
       ])
       .where('partyUser.userId = :userId', { userId })
@@ -38,15 +38,6 @@ export class GetMyPartiesHandler implements IQueryHandler<GetMyPartiesQuery> {
       .orderBy(`partyUser.${sort}`, order)
       .getManyAndCount();
 
-    // parties.forEach((party) => {
-    //   if (party.status === 'archived') {
-    //     party['tag'] = '종료';
-    //   } else if (party.status === 'active') {
-    //     party['tag'] = '진행중';
-    //   }
-    // });
-
-    // 결과를 응답 형식에 맞춰 반환
     return {
       total,
       partyUsers,
