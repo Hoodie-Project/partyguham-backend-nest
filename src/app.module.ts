127101a1bcca2724e8aa5083544c1875023dceac
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-03-13 17:37:18 +0900

    update: guild module, import 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,17 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
-
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { ResponseInterceptor } from './common/interceptor/response';
-
-import { AppController } from './app.controller';
-import { UserModule } from './user/user.module';
-import { AppService } from './app.service';
-import { PartyModule } from './party/party.module';
-import { SkillModule } from './skill/skill.module';
-import { PositionModule } from './position/position.module';
-import { LocationModule } from './location/location.module';
-import { PersonalityModule } from './personality/personality.module';
-import { GuildModule } from './guild/guild.module';
 import { NotFoundExceptionFilter } from './common/exception/error-NotFound.filter';
 import { CustomErrorExceptionFilter } from './common/exception/error.filter';
 import { UnauthorizedExceptionFilter } from './common/exception/error-Unauthorized.filter';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { UserModule } from './user/user.module';
 import { ReportModule } from './report/report.module';
 import { AuthModule } from './auth/auth.module';
 import { BannerModule } from './banner/banner.module';
