8914c098ea63c90fd18438c27c0247990a1c2d90
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-04-22 23:33:35 +0900

    update: 파티 생성시 이미지 경로

--- src/party/application/command/create-party.comand.ts
+++ src/party/application/command/create-party.comand.ts
@@ -5,7 +5,7 @@ export class CreatePartyCommand implements ICommand {
     readonly userId: number,
     readonly title: string,
     readonly content: string,
-    readonly image: string,
+    readonly imagePath: string,
     readonly partyTypeId: number,
     readonly positionId: number,
   ) {}
--- src/party/application/command/create-party.handler.ts
+++ src/party/application/command/create-party.handler.ts
@@ -6,25 +6,30 @@ import { PartyFactory } from 'src/party/domain/party/party.factory';
 import { IPartyRepository } from 'src/party/domain/party/repository/iParty.repository';
 import { IPartyUserRepository } from 'src/party/domain/party/repository/iPartyUser.repository';
 import { IPartyTypeRepository } from 'src/party/domain/party/repository/iPartyType.repository';
+import { ImageService } from 'src/libs/image/image.service';
 
 @Injectable()
 @CommandHandler(CreatePartyCommand)
 export class CreatePartyHandler implements ICommandHandler<CreatePartyCommand> {
   constructor(
     private partyFactory: PartyFactory,
+    private imageService: ImageService,
     @Inject('PartyRepository') private partyRepository: IPartyRepository,
     @Inject('PartyTypeRepository') private partyTypeRepository: IPartyTypeRepository,
     @Inject('PartyUserRepository') private partyUserRepository: IPartyUserRepository,
   ) {}
 
   async execute(command: CreatePartyCommand) {
-    const { userId, title, content, image, partyTypeId, positionId } = command;
+    const { userId, title, content, imagePath, partyTypeId, positionId } = command;
+
     const partyType = await this.partyTypeRepository.findOne(partyTypeId);
     if (!partyType) {
       throw new NotFoundException('Party Type이 존재하지 않습니다.', 'NOT_FOUND');
     }
 
-    const party = await this.partyRepository.create(partyTypeId, title, content, image);
+    const savedImagePath = this.imageService.getRelativePath(imagePath);
+
+    const party = await this.partyRepository.create(partyTypeId, title, content, savedImagePath);
 
     await this.partyUserRepository.createMaster(userId, party.id, positionId);
 
