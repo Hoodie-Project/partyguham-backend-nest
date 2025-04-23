71ff8025cbdd1ca83c8e6338881ae3fd06a8356b
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-04-23 14:04:23 +0900

    docs: fcm 문서 수정

--- src/libs/firebase/dto/fcm-test.dto.ts
+++ src/libs/firebase/dto/fcm-test.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsIn, IsNotEmpty, IsString } from 'class-validator';
 
-export class RegisterFcmTokenDto {
+export class FcmTestDto {
   @ApiProperty({
     example: 'FCM TOKEN',
     description: '업로드할 유저(디바이스) 토큰',
@@ -11,12 +11,18 @@ export class RegisterFcmTokenDto {
   fcmToken: string;
 
   @ApiProperty({
-    example: 'android, ios, chrome',
-    enum: ['android', 'ios', 'chrome'],
-    description: '토큰 주체',
+    example: '제목',
+    description: '푸시 알람 제목',
   })
   @IsString()
   @IsNotEmpty()
-  @IsIn(['android', 'ios', 'chrome'])
-  device: string;
+  title: string;
+
+  @ApiProperty({
+    example: 'message(body)',
+    description: '푸시 알람 내용',
+  })
+  @IsString()
+  @IsNotEmpty()
+  message: string;
 }
