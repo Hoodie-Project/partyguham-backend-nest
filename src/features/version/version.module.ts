e189d9089ceaf13578caf29df0cc803a4a708dac
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-05-25 01:58:56 +0900

    feat: 버전 확인 api 추가

--- src/features/version/version.module.ts
+++ src/features/version/version.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { VersionController } from './version.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { VersionService } from './version.service';
+import { VersionController } from './version.controller';
+import { VersionEntity } from './entity/version.entity';
+import { VersionRepository } from './repository/version.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([VersionEntity])],
+  providers: [VersionService, VersionRepository],
   controllers: [VersionController],
-  providers: [VersionService]
 })
 export class VersionModule {}
