9789cc1efa4b04687656fe3aaa805566d11b8a0e
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-06-07 22:38:28 +0900

    feat: aws_s3 이미지 업데이트 기능 추가

---
+++ src/libs/aws/s3/s3.controller.ts
@@ -0,0 +1,19 @@
+// upload.controller.ts
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { S3Service } from './s3.service';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly s3Service: S3Service) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('image'))
+  async upload(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('파일이 업로드되지 않았습니다.');
+    }
+    const url = await this.s3Service.uploadFile(file);
+    return { url };
+  }
+}
