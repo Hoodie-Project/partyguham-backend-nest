40a48a18e3da18f531a04348fc80fafa33360ca8
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-08-24 14:01:41 +0900

    update: k6 script

--- test/k6/script.js
+++ test/k6/script.js
@@ -3,14 +3,25 @@ import { check, sleep } from 'k6';
 
 // 부하 테스트 설정
 export let options = {
-  vus: 100, // 동시에 접속할 가상 유저 수
+  vus: 1, // 동시에 접속할 가상 유저 수
   duration: '10m', // 테스트 지속 시간
 };
 
 export default function () {
-  let res = http.get('https://partyguham.com');
+  const refreshToken =
+    'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImZzY2lGRDV3N3RRVFdjWWl2SFozb1E9PSIsImlhdCI6MTczNzAzMzE5NSwiZXhwIjoxNzM5NjI1MTk1fQ.xGVAE6UDiW39uVEhV5mr-AW_I4lNIhEB_zv9hiSpyVdMZwbq8L_GwMjqRrrw2Rg9iTEjeDyY_7HHhwhT9HgM-g'; // 실제 토큰 값
+  const headers = {
+    Authorization: `Bearer ${refreshToken}`,
+  };
+  const cookies = {
+    refreshToken: refreshToken,
+  };
+
+  let res = http.post('https://partyguham.com/api/auth/access-token', null, { headers });
+
   check(res, {
-    '응답 코드가 200인가?': (r) => r.status === 200,
+    '응답 코드가 201인가?': (r) => r.status === 201,
   });
+
   sleep(1);
 }
